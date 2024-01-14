7b861e4a2f7fe0e80d0d1eb613d74e101639d5f2
guerchoig <guerchoig@gmail.com>
2024-01-14 17:52:34 +0300

    Ver 2

--- lambda.cpp
+++ lambda.cpp
@@ -1,9 +1,27 @@
-#include <vector>
-#include <iostream>
-#include <algorithm>
+#include <string>
+#include <tuple>
+#include <cassert>
+auto getPerson() {
+ const std::string name = "Petia";
+ const std::string secondName = "Ivanoff";
+ const std::size_t age = 23;
+ const std::string department = "Sale";
+ return std::make_tuple(
+ name, secondName, age, department
+ );
+}
 
-int main(void){
-std::vector<int> i{1, 2, 3, 4};
-std::for_each(i.begin(), i.end(), [](int x){std::cout << x << '\n';});
+std::tuple<std::string&, std::string&, size_t&, std::string&> custom_tie(std::string& name, std::string& secondName, size_t& age, std::string& department){
+ return {name, secondName, age, department };
+}
 
+int main(void) {
+ std::string name, secondName, department;
+ std::size_t age;
+ custom_tie(name, secondName, age, department) = getPerson();
+ assert(name == "Petia");
+ assert(secondName == "Ivanoff");
+ assert(age == 23);
+ assert(department == "Sale");
+ return 0;
 }
